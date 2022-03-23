3c28a7447e27aaed5889b4c598f448e88b5048d9
Siddharth <93863822+sidstargem@users.noreply.github.com>
2022-03-23 09:46:39 +0000

    Update home.js

--- script/home.js
+++ script/home.js
@@ -10,6 +10,26 @@ $('#nav + button .close').click(function(){
     $('.close').hide();
   });
 
+$("#submit").on('click', function() {   
+    var phone = jQuery("#phone").val(); 
+    var name = jQuery("#first-name").val(); 
+    var mail = jQuery("#email").val();  
+    var message = jQuery("#message").val(); 
+    alert(phone); 
+    alert(name); 
+    alert(mail); 
+    alert(message); 
+
+    jQuery.post("https://thegoodtoknow.co.uk/sendmail.php",
+    {name: name, mail: mail , message: message}, 
+    function(data){
+        jQuery("#email").html("<div id='sent'><p>Thank you for the message.</p><p>We will reply as soon as possible.</p></div>");
+        alert('sent');
+    });      
+
+return false;       
+});  
+
 $('a[href*=\\#]').on('click', function() { 
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
